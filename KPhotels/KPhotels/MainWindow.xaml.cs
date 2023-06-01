1f2ff6212bbba438e894a0a35020d0515ccac9e2
AnnaKlevtsova <122230781+AnnaKlevtsova@users.noreply.github.com>
2023-06-01 09:46:52 +0300

    Add files via upload

---
+++ KPhotels/KPhotels/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+
+ namespace KPhotels
+{
+    public partial class MainWindow : Window
+    {
+        public string conString = @"Data Source=.\SQLEXPRESS; Initial Catalog=databasehotels; Integrated Security=true;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Content = new administration1();
+            administration1.conString = conString;
+            administration2.conString = conString;
+            clients1.conString = conString;
+            Numbers1.conString = conString;
+            registration.conString = conString;
+
+
+        }
+
+        //по нажатию на кнопку возврат на главное меню
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            MainFrame.Navigate(new administration1());
+            
+        }
+        //скрытие кнопки в зависимости от страницы
+        private void Button_Exit(object sender, EventArgs e)
+        {
+
+            if (MainFrame.CanGoBack)
+            {
+                ButtonExit.Visibility = Visibility.Visible;
+            }
+            //проверка, открыта ли страница входа, если да, то кнопка Выход скрыта
+            if (MainFrame.Content.ToString() == "KPhotels.administration1")
+            {
+            ButtonExit.Visibility = Visibility.Hidden;
+            }
+        }
+      
+    }
+}
---
+++ KPhotels/KPhotels/clients2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+namespace KPhotels
+{
+    public partial class clients2 : Page
+    {
+        public clients2()
+        {
+            InitializeComponent();
+            //скрываем кнопку
+            buttonchooseanumber.Visibility = Visibility.Hidden;
+            l.Content += " "+clients1.namehotel;
+            //помечаем, что даты, которые уже прошли, выбрать нельзя
+            date.BlackoutDates.AddDatesInPast();
+        }
+
+
+        public static string start { get; set; }
+        public static string finish { get; set; }
+        public static int duration { get; set; }
+    //выбор даты/дат
+    private void date_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+           //получаем первую выбранную дату
+            start = Convert.ToString(date.SelectedDates.First());
+            //обрезаем дату
+            start = start.Remove(start.Length - 8);
+            //получаем последнюю выбранную дату
+            finish = Convert.ToString(date.SelectedDates.Last());
+            finish = finish.Remove(finish.Length - 8);
+            duration= date.SelectedDates.Count();
+            //после выбора даты можно перейти к выбору номера, поэтому кнопка отображается
+            buttonchooseanumber.Visibility = Visibility.Visible;
+        }
+
+        //переход на новую страницу
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Numbers1());
+        }
+
+        //переход на предыдущую страницу
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new clients1());
+        }
+    }
+}
